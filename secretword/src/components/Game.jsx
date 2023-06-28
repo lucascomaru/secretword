b59891fd70ab74086cd70e770dfc8040dd91e3ce
Lucas Comaru <lucascomaru20@gmail.com>
2023-06-28 00:15:41 -0300

    Implementando eventos

--- secretword/src/components/Game.jsx
+++ secretword/src/components/Game.jsx
@@ -1,10 +1,47 @@
 import './Game.css'
 
-const Game = ({verifyLetter}) => {
+const Game = ({verifyLetter,
+     pickedWord,
+      pickedCategory,
+       letters,
+        guessedLetters, 
+        wrongLetters,
+        guesses,
+        score,
+    }) => {
   return (
-    <div>
-        <h1>Game</h1>
-        <button onClick={verifyLetter}>Finalize jogo</button>
+    <div className="game">
+        <p className="points">
+            <span>Pontuação: {score}</span>
+        </p>
+        <h1>Adivinhe a palavra: </h1>
+        <h3 className="tip">
+            Dica sobre a palavra: <span>{pickedCategory}</span>
+        </h3>
+        <p>Você tem {guesses} tentativas(s).</p>
+        <div className="wordContainer">
+            {letters.map((letter,i) => (
+                guessedLetters.includes(letter) ? (
+                    <span key={i} className="letter">{letter}</span>
+                ) : (
+                    <span key={i} className="blankSquare"></span>
+                )
+            ))}
+            <span className="blackSquare"></span>
+        </div>
+        <div className="letterContainer">
+            <p>Tente adivinhar uma letra da palavra: </p>
+            <form>
+                <input type="text" name = "letter" maxLength="1" required />
+                <button>Jogar!</button>
+            </form>
+        </div>
+        <div className="wrongLetterContainer">
+            <p>Letras já utilizadas: </p>
+            {wrongLetters.map((letter, i) =>(
+                <span key={i}>{letter}, </span>
+            ))}
+        </div>
     </div>
   )
 }
